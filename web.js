a2a4d3c357780e6de41ce896b96bedd362fc0a84
Lewis Ritz <lewisritz@lewisritz.com>
2014-01-14 20:47:33 -0500

    testing the waters with some tutorials

--- web.js
+++ web.js
@@ -1,15 +1,10 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
 var app = express();
 
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-	res.send('<h1 style=\"text-align:center\">Hello World!</h1>');
+app.get('/', function(req, res){
+	res.send('<h1 style="text-align:center">Hello World!</h1>');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-console.log("Listening on " + port);
+app.listen(5000, function(){
+	console.log("Listening on Port 5000.."); 
 });
