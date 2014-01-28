645e5c3a2bf7b9766b9c086ccbbaa573704a5c11
Lewis Ritz <lewisritz@lewisritz.com>
2014-01-28 12:03:58 -0500

    working through Node Beginner Tutorials

--- my_static_file_server.js
+++ my_static_file_server.js
@@ -1,8 +1,11 @@
 var express = require('express'),
+	http = require('http'),
 	app = express();
+
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 4000);
 
-console.log("App listening on port 5000");
+console.log("http: "+http.toString());
+console.log("App listening on port 4000");
---
+++ server.js
@@ -0,0 +1,42 @@
+/*
+var http = require("http");
+
+http.createServer(function(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello World");
+	response.end();
+}).listen(8888);
+*/
+/*
+function say(word) {
+	console.log(word);
+}
+
+function execute(someFunction, value) {
+	someFunction(value);
+}
+
+execute(say, "Hello");
+*/
+/*
+function execute(someFunction, value){
+	someFunction(value);
+}
+execute(function(word){ console.log(word) }, "Hello");
+*/
+var http = require("http");
+var url = require("url");
+
+function start(route, handle){
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+
+		route(handle, pathname, response);
+	}
+
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
+}
+
+exports.start = start;
