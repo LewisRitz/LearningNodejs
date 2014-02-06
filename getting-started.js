49bbce1bf4f5e5b91705cf1825036373df343ab0
Lewis Ritz <lewisritz@lewisritz.com>
2014-02-05 22:31:30 -0500

    starting up with mongoose and mongoDB

---
+++ getting-started.js
@@ -0,0 +1,43 @@
+// getting-started.js
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback(){
+		//yay!
+		//console.log("IT WORKED!");
+		var kittySchema = mongoose.Schema({
+			name: String
+		});
+		kittySchema.methods.speak = function() {
+			var greeting = this.name
+				? "Meow name is " + this.name
+				: "I don't have a name"
+			console.log(greeting);
+		}
+
+		var Kitten = mongoose.model('Kitten', kittySchema);
+
+		var silence = new Kitten({ name: 'Silence'});
+		//console.log(silence.name);
+		var fluffy = new Kitten({ name: 'fluffy' });
+		//fluffy.speak();
+		//silence.speak();
+		fluffy.save(function (err, fluffy){
+			if (err) { console.log("Save Error: "+err); }
+			fluffy.speak();
+		});
+		silence.save(function (err, silence){
+			if (err) { console.log("Save Error: "+err); }
+			silence.speak();
+		});
+
+		
+		Kitten.find(function (err, kittens){
+			if (err) { console.log(err); }//TODO handle err
+			console.log(kittens);
+		});
+		
+		//Kitten.find({ name: /^Fluff/ }, callback);
+});
